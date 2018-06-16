724f9f887d67bd5f9e3ac12e91ec7d3a8a491de5
Kendrick Williams <kendrick.williams@coxautoinc.com>
2018-06-16 13:14:08 -0400

    Added some boilerplate for test

--- test/happy.test.js
+++ test/happy.test.js
@@ -1,1 +1,7 @@
 import happy from '../src/happy.js';
+
+describe('happy', () => {
+    it('should be defined');
+    it('should take an Error object');
+    it('should return object with message parameter');
+});
